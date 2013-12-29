125518d8761ebe722cafd6936aea96741af782d0
Arielle Simmons <ari.ucb.fire@gmail.com>
2013-12-29 00:18:50 -0800

    Commit Initial Code/Function

---
+++ Create_Points_Along_Line.py
@@ -0,0 +1,52 @@
+###############################################
+## Arielle Simmons                           ##
+## GIS Data Engineer                         ##
+## Tableau Software                          ##
+## Date created: December 25, 2013           ##
+## Date modified: December 28, 2013          ##
+###############################################
+
+## This script is to create points at specific intervals
+## along a measured line segment
+## returns nothing if the line is too short according to
+## user definition
+## to call the function 'createPointsAtInterval'
+## use : createPointsAtInterval('<name of line fc>', interval, shortestLineLength)  
+
+
+import arcpy
+from arcpy import env
+import math
+
+## declare your env.workspace as the geodatabase you are working in
+env.workspace = r'<path/name.gdb>'
+
+def createPointsAtInterval(inLine, interval, shortestLineLength):
+    sr = arcpy.Describe(inLine).spatialReference
+    inLineName = arcpy.Describe(inLine).name
+    segPts = arcpy.CreateFeatureclass_management(env.workspace, inLineName + '_pts', 'POINT','','','',sr)
+    icursor = arcpy.da.InsertCursor(segPts, ('SHAPE@'))
+    with arcpy.da.SearchCursor(inLine, ("SHAPE@LENGTH", "SHAPE@")) as cursor:
+        for row in cursor:
+            length = row[0]
+            # measures length of the line, determines number of point intervals
+            numIntervals = int(math.floor(length / interval))
+            lastSegLength = length - interval * numIntervals
+            for x in range(1, numIntervals):
+                # newPt = row[1].positionAlongLine(.50,True).firstPoint
+                newPt = row[1].positionAlongLine(interval * x)
+                icursor.insertRow((newPt,))
+            # if line length is greater then shortestlin only create one endpoint
+            # otherwise leave blank
+            if length > shortestLineLength:
+                lastPt = row[1].positionAlongLine(interval * numIntervals)
+                icursor.insertRow((lastPt,))
+    return segPts
+
+        
+
+
+
+
+
+    
---
+++ README.md
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+</head>
+<body>
+# Table of Contents
+[Team Members](#team-members)
+
+[Project Summary](#project-summary)
+
+# <a name="team-members"></a>Team Members
+* "Arielle Simmons" <ari.ucb.fire@gmail.com>
+	- Data Engineer 
+	
+# <a name="project-summary"></a>Project Summary
+
+Arcpy (10.1/10.2) function using 'positionAlongLine' to create points
+at a specified interval. Also includes ignores any lines the user considers 
+too short.
+
+
+
+*NOTE: THIS PROJECT IS STILL in progress as of 12/29/2013*
+ 
+</body>
+</html>
